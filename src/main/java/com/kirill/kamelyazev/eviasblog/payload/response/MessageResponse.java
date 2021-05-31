c033cef552d68921324ac574001ea62affb11f6c
Somellin <kirill.kamelyazev@gmail.com>
2021-05-31 21:03:38 +0700

    first commit

--- src/main/java/com/kirill/kamelyazev/eviasblog/payload/response/MessageResponse.java
+++ src/main/java/com/kirill/kamelyazev/eviasblog/payload/response/MessageResponse.java
@@ -6,11 +6,9 @@ public class MessageResponse {
     public MessageResponse(String message) {
         this.message = message;
     }
-
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
     }
